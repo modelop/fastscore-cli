2255cc1edeaa87c28b4edac12704635765501a3b
Maxim Kharchenko <maxim.kharchenko@gmail.com>
2016-08-21 17:14:16 +0000

    Connects to notify websocket in interactive mode

--- cmd/connect.py
+++ cmd/connect.py
@@ -1,6 +1,8 @@
 
 import requests
 
+import fastscore
+
 def main(args):
   prefix = args["url-prefix"]
   r = requests.get(prefix + "/1/swagger", verify=False)
@@ -8,5 +10,6 @@ def main(args):
   assert swagger["info"]["title"] == "Connect API"
   with open(".fastscore", "w") as f:
     f.write("connect-prefix: %s\n" % prefix)
+  fastscore.conf["connect-prefix"] = prefix
   print "Connect API: ok"
 
--- interactive.py
+++ interactive.py
@@ -1,6 +1,10 @@
 
 import readline
+import thread
+import ssl
+from websocket import create_connection
 
+import fastscore
 import dispatch
 
 words = []
@@ -14,14 +18,27 @@ def complete(text, state):
   return None
 
 def loop():
+  if "connect-prefix" in fastscore.conf:
+    connect_notify()
+    
   readline.set_completer(complete)
   readline.parse_and_bind("tab: complete")
   try:
     while True:
       line = raw_input("> ")
-      if line != []:
+      if line != "":
         if not dispatch.run(line.split()):
           print "Invalid command - use 'help'"
   except EOFError:
     print
 
+def connect_notify():
+  prefix = fastscore.conf["connect-prefix"]
+  def notify():
+    ws = create_connection(prefix.replace("https:", "wss:") + "/1/notify",
+                              sslopt={"cert_reqs": ssl.CERT_NONE})
+    while True:
+      x = ws.recv()
+      print x
+  thread.start_new_thread(notify, ())
+  
