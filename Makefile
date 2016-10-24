a560488dd9cffc9cac74944c31b009626781745f
Maxim Kharchenko <maxim.kharchenko@gmail.com>
2016-10-24 08:47:30 +0000

    Changes the version to 'dev'

--- Makefile
+++ Makefile
@@ -3,5 +3,5 @@
 
 default:
 	python setup.py sdist
-	aws s3 cp --acl public-read dist/fastscore-cli-1.1.tar.gz s3://fastscore-cli/
+	aws s3 cp --acl public-read dist/fastscore-cli-dev.tar.gz s3://fastscore-cli/
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
   name = "fastscore-cli",
   description = "FastScore CLI",
-  version = "1.1",
+  version = "dev",
   packages = find_packages(),
   
   install_requires = [
