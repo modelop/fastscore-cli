9136eff785fb52d3e7e26f6494b06f62813a931f
Maxim Kharchenko <maxim.kharchenko@gmail.com>
2017-01-18 05:13:05 +0000

    Adds -wait option to 'fastscore connect'

--- fastscore/connect.py
+++ fastscore/connect.py
@@ -1,12 +1,34 @@
 
+import sys
+
 import requests
+from time import sleep
 
 import service
 
 def main(args):
   prefix = args["url-prefix"]
-  r = requests.get(prefix, verify=False)
-  assert r.status_code == 200
+  connect_health = prefix + "/api/1/service/connect/1/health"
+
+  if service.options["wait"]:
+    sys.stdout.write("Connecting...")
+    sys.stdout.flush()
+    while True:
+      try:
+        r = requests.get(connect_health, verify=False)
+        if r.status_code == 200:
+          break
+        progress_char = ":"
+      except requests.ConnectionError:
+        progress_char = "."
+      sys.stdout.write(progress_char)
+      sys.stdout.flush()
+      sleep(0.5)
+    print " done"
+  else:
+    r = requests.get(prefix, verify=False)
+    assert r.status_code == 200
+
   with open(".fastscore", "w") as f:
     f.write("proxy-prefix: %s\n" % prefix)
   service.options["proxy-prefix"] = prefix
